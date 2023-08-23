ecea29bf123a5ee1b7b605f2f05e576f5a07d677
guilherme-maciel99 <75001340+guilherme-maciel99@users.noreply.github.com>
2023-08-23 14:57:07 -0300

    primeiro commit

---
+++ src/main/java/org/br/serverest/ServerestApplication.java
@@ -0,0 +1,13 @@
+package org.br.serverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerestApplication.class, args);
+	}
+
+}
