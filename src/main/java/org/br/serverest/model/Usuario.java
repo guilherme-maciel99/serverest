74441b5f0488e183281982894ca6407cc9df062f
guilherme-maciel99 <75001340+guilherme-maciel99@users.noreply.github.com>
2023-08-24 20:35:52 -0300

    Adicionando anotações

--- src/main/java/org/br/serverest/model/Usuario.java
+++ src/main/java/org/br/serverest/model/Usuario.java
@@ -2,10 +2,14 @@ package org.br.serverest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
     private String nome;
     private String email;
